fe1211a22721abd01eca0e29d31a860bbb426fd4
Alex Anderson <alex@frontlinesms.com>
2013-04-16 18:15:33 +0300

    Initial commit

---
+++ MultiTenantSingleDbQuartz2IntegrationGrailsPlugin.groovy
@@ -0,0 +1,52 @@
+import org.quartz.JobExecutionContext
+import grails.plugin.quartz2.GrailsArtefactJob
+import grails.plugin.multitenantsingledbquartz2integration.*
+
+class MultiTenantSingleDbQuartz2IntegrationGrailsPlugin {
+	def version = "0.1-SNAPSHOT"
+	def grailsVersion = "2.0 > *"
+	def pluginExcludes = ['grails-app/views/error.gsp']
+	def title = 'Multi Tenant Single Db Quartz2 Integration Plugin'
+	def author = 'Alex Anderson'
+	def authorEmail = ''
+	def description = 'Integration between Quartz2 and Multi-Tenant-Single-Db plugins to allow jobs to be run in the context of a Tenant.'
+	def documentation = 'http://grails.org/plugin/multi-tenant-single-db-quartz2-integration'
+	def license = 'APACHE'
+	def loadAfter = ['quartz2']
+	def organization = [name:'FrontlineSMS', url:'http://www.frontlinesms.com']
+	def issueManagement = [system:'github', url:'https://github.com/frontlinesms/grails-multi-tenant-single-db-quartz2-integration-plugin/issues']
+	def scm = [url:'git@github.com:frontlinesms/grails-multi-tenant-single-db-quartz2-integration-plugin.git']
+
+	private def withCurrentTenantAwareness(def currentTenant, Map params, Closure c) {
+		params['grails-multi-tenant-single-db-tenant-id'] = currentTenant.get()
+		c.call(params)
+	}
+
+	def doWithSpring = {
+		quartzJobFactory(TenantAwareJobFactory)
+	}
+
+	def doWithDynamicMethods = { ctx ->
+		def currentTenant = ctx.getBean('currentTenant')
+
+		def grailsArtefactJobMetaClass = GrailsArtefactJob.metaClass
+		def oldExecute = grailsArtefactJobMetaClass.getMetaMethod('execute', [JobExecutionContext] as Class[])
+		grailsArtefactJobMetaClass.execute = { JobExecutionContext jec ->
+			currentTenant.set(jec.mergedJobDataMap['grails-multi-tenant-single-db-tenant-id'])
+			oldExecute.invoke(jec)
+		}
+
+		application.jobClasses.each { jc ->
+			def oldMethod = jc.metaClass.getStaticMetaMethod('triggerNow', [Map] as Class[])
+			jc.metaClass.static.triggerNow = { Map params = null ->
+				params = params?: [:]
+				withCurrentTenantAwareness currentTenant, params, {
+					oldMethod.invoke(null, params)
+				}
+			}
+
+			// TODO decorate all the other job scheduling methods as well
+		}
+	}
+}
+
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,25 @@
+grails.project.work.dir = 'target'
+grails.project.target.level = 1.6
+
+grails.project.dependency.resolution = {
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsCentral()
+	}
+
+	plugins {
+		build ':release:2.2.1',
+				':rest-client-builder:1.0.3', {
+			export = false
+		}
+
+		compile ':quartz2:2.1.6.2',
+				':multi-tenant-single-db:0.8.2',
+				":hibernate:$grailsVersion", {
+			export = false
+		}
+	}
+}
+
