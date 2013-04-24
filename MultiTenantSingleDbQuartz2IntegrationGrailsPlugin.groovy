6b17e045a3366f6b375f50a551a62dfb55f26a01
Alex Anderson <alex@frontlinesms.com>
2013-04-24 17:05:56 +0300

    Release v0.1

--- MultiTenantSingleDbQuartz2IntegrationGrailsPlugin.groovy
+++ MultiTenantSingleDbQuartz2IntegrationGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import grails.plugin.multitenantsingledbquartz2integration.*
 
 class MultiTenantSingleDbQuartz2IntegrationGrailsPlugin {
-	def version = "0.1-SNAPSHOT"
+	def version = "0.1"
 	def grailsVersion = "2.0 > *"
 	def pluginExcludes = ['grails-app/views/error.gsp']
 	def title = 'Multi Tenant Single Db Quartz2 Integration Plugin'
